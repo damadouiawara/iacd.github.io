ab4412cbc7849c6c772509f8d60ca077f27e9646
Amadou Diawara <damadouiawara@gmail.com>
2023-12-30 17:58:22 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>IACD CONSULTING</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora">
+    <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="assets/fonts/fontawesome5-overrides.min.css">
+    <link rel="stylesheet" href="assets/css/Article-Clean.css">
+    <link rel="stylesheet" href="assets/css/Article-List.css">
+    <link rel="stylesheet" href="assets/css/Elegant-Registration-Form.css">
+    <link rel="stylesheet" href="assets/css/Footer-Dark.css">
+    <link rel="stylesheet" href="assets/css/Login-Form-Clean.css">
+    <link rel="stylesheet" href="assets/css/Ludens-Users---2-Simple-Registration-Section.css">
+    <link rel="stylesheet" href="assets/css/Navigation-Clean-1.css">
+    <link rel="stylesheet" href="assets/css/Navigation-Clean.css">
+    <link rel="stylesheet" href="assets/css/Navigation-with-Button.css">
+    <link rel="stylesheet" href="assets/css/Pretty-Registration-Form.css">
+    <link rel="stylesheet" href="assets/css/Projects-Clean.css">
+    <link rel="stylesheet" href="assets/css/Registration-Form-with-Photo.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body style="background: var(--bs-dark);">
+    <nav class="navbar navbar-light navbar-expand-lg navigation-clean" style="background: linear-gradient(120deg, var(--bs-purpleb) 0%, var(--bs-purpleb) 52%, var(--bs-white) 100%);">
+        <div class="container"><a class="navbar-brand" href="index.html" style="font-size: 25px;color : white; ">IACD BARACK OBAMA</a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navcol-1" style="border-style: none;">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"></li>
+                    <li class="nav-item"></li>
+                    <li class="nav-item"></li>
+                    <li class="nav-item"><a class="nav-link" href="identifier.html" style="font-size: 20px;background: var(--bs-purpleb);border-style: groove;border-radius: 35px; color :white;"><strong>S'identifier</strong></a></li>
+                    <li class="nav-item"><a class="nav-link" href="inscrire.html" style="font-size: 20px;background: var(--bs-purpleb);border-style: groove;border-radius: 35px;color : white; "><strong>S'inscrire</strong></a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    
+    <section class="article-clean"></section>
+    <nav class="navbar navbar-light navbar-expand-lg navigation-clean-button" style="background: linear-gradient(180deg, var(--bs-white) 51%, var(--bs-purpleb) 100%);">
+        <div class="container"><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-2"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navcol-2">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item"></li>
+                    <li class="nav-item"><a class="nav-link" href="membre.html" style="color: var(--bs-dark);"><strong>NOS MEMBRES</strong></a></li>
+                    <li class="nav-item"><a class="nav-link" href="projet.html" style="color: var(--bs-dark);"><strong>NOTRE PROJET</strong></a></li>
+                    <li class="nav-item"></li>
+                    <li class="nav-item"></li>
+                    <li class="nav-item"><a class="nav-link" href="partenaire.html" style="color: var(--bs-dark);"><strong>PARTENAIRE</strong></a></li>
+                    <li class="nav-item"><a class="nav-link" href="service.html" style="color: var(--bs-dark);"><strong>SERVICE</strong></a></li>
+                    <li class="nav-item"><a class="nav-link" href="contact.html" style="color: var(--bs-dark);"><strong>CONTACT</strong></a></li>
+                    <li class="nav-item"><a class="nav-link" href="propos.html" style="color: var(--bs-dark);"><strong>A PROPOS</strong></a></li>
+                    <li class="nav-item"><a class="nav-link" href="politic.html" style="color: var(--bs-dark);"><strong>NOS REGLES</strong></a></li>
+                    <li class="nav-item"></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <section class="article-clean">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-10 col-xl-8 offset-lg-1 offset-xl-2">
+                    <div class="intro">     
+                        <h1 class="text-center">IACD BARACK OBAMA</h1>
+                        <p class="text-center"> </p>
+                    </div>
+                    <div class="iacd_logo">
+                        <img src="assets/img/iacd_logo.png" style ="width : 180px; height: 180px;border-radius : 110px;">
+
+                    </div>
+                    
+                </div>
+                
+                <div class="col">
+                    <section class="article-list">
+                        <div class="container">
+                            <div class="intro">
+                                <h2 class="text-center">Dernier Evenements</h2>
+                                <p class="text-center">Nunc luctus in metus eget fringilla. Aliquam sed justo ligula. Vestibulum nibh erat, pellentesque ut laoreet vitae. </p>
+                            </div>
+                            <div class="row articles">
+                                <div class="col-sm-6 col-md-4 item"><a href="#"><img class="img-fluid" src="assets/img/desk.jpg"></a>
+                                    <h3 class="name">Article Title</h3>
+                                    <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, interdum justo suscipit id.</p><a class="action" href="#"><i class="fa fa-arrow-circle-right"></i></a>
+                                </div>
+                                <div class="col-sm-6 col-md-4 item"><a href="#"><img class="img-fluid" src="assets/img/building.jpg"></a>
+                                    <h3 class="name">Article Title</h3>
+                                    <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, interdum justo suscipit id.</p><a class="action" href="#"><i class="fa fa-arrow-circle-right"></i></a>
+                                </div>
+                                <div class="col-sm-6 col-md-4 item"><a href="#"><img class="img-fluid" src="assets/img/loft.jpg"></a>
+                                    <h3 class="name">Article Title</h3>
+                                    <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, interdum justo suscipit id.</p><a class="action" href="#"><i class="fa fa-arrow-circle-right"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer class="footer-dark">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6 col-md-3 item">
+                    <h3>Services</h3>
+                    <ul>
+                        <li><a href="#">Web design</a></li>
+                        <li><a href="#">Development</a></li>
+                        <li><a href="#">Hosting</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-6 col-md-3 item">
+                    <h3>About</h3>
+                    <ul>
+                        <li><a href="#">Company</a></li>
+                        <li><a href="#">Team</a></li>
+                        <li><a href="#">Careers</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-6 item text">
+                    <h3>IACD CONSULTING</h3>
+                    <p>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit pulvinar dictum vel in justo.</p>
+                </div>
+                <div class="col item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="fab fa-instagram"></i></a><a href="#"><i class="icon ion-social-linkedin"></i></a></div>
+            </div>
+            <p class="copyright">IACD Consulting © 2023</p>
+        </div>
+    </footer>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
